82b06dd5f7311f750677e9a9ec60188f4e67d388
Sxvxgee <aboud1xy@gmail.com>
2022-06-17 14:13:12 +0400

    Revamped all events, started using tables.

--- cogs/events.py
+++ cogs/events.py
@@ -5,14 +5,14 @@ from discord.ext.commands import Cog
 
 from utils.useful import custom, get_data, success, failure
 
-
+from rich import box, print
+from rich.table import Table
 
 class events(commands.Cog):
   """Events handled by the bot."""
   def __init__(self, bot : discord.Client):
     self.bot = bot
     self.hidden = True
-    self.data = get_data()
     self.gift_regex = re.compile("(discord.com/gifts/|discordapp.com/gifts/|discord.gift/)([a-zA-Z0-9]+)")
     self.link_regex = re.compile("http://.*..*|https://.*..*")
     self.scam_links = requests.get("https://raw.githubusercontent.com/nikolaischunk/discord-phishing-links/main/domain-list.json").json()
@@ -20,10 +20,13 @@ class events(commands.Cog):
 
   @Cog.listener("on_message")
   async def nitro_snipe(self, message):
+    data = get_data()
+    if data['nitrosnipe'] is False:
+      return
     if self.gift_regex.search(message.clean_content):
       code = self.gift_regex.search(message.clean_content).group(2)
       headers = {
-        'Authorization': self.data['token'],
+        'Authorization': data['token'],
         'user-agent': 'Mozilla/5.0'
       }
       json_ = {
@@ -35,41 +38,94 @@ class events(commands.Cog):
             json=json_,
             headers=headers
         )
-        success(f"Sniped code: {code}", header = 'Nitro Sniper')
+        table = Table(
+          title="Nitro Sniper \[Guild]" if isinstance(message.channel, discord.TextChannel) else "Nitro Sniper \[DM]",
+          caption = f"Run {data['prefix']}nitrosnipe to disable this feature\n",
+          box = box.HEAVY,
+          style = "bold green"
+        )
         data = response.json()
-        if 'This gift has been redeemed already' in data['message']:
-          custom('[Nitro Sniper] The code has already been used previously.')
-        elif 'Unknown Gift Code' in data['message']:
-          custom('[Nitro Sniper] The code is invalid.')
-        else:
-          custom(f'[Nitro Sniper] {data["message"]}')
+        table.add_column("Nitro Code", justify = "center")
+        table.add_column("Nitro Redeem Response", justify = "center")
+        table.add_column("Message ID", justify = "center")
+        table.add_row(code, data['message'], str(message.id))
+        print(table)
 
   @Cog.listener("on_message_delete")
   async def deleted_log(self, message : discord.Message):
-    if message.channel.id in self.data['blacklisted_channels']:
+    data = get_data()
+    if data['deleted'] is False:
       return
-    if (len(message.clean_content) > 0
-    and message.author.id != self.bot.user.id
-    and message.author.id not in self.data['owner_ids']):
-      if (message.channel.id in self.data['logged_channels']
-      or message.guild.id in self.data['logged_guilds']):
-        msg = f"{message.clean_content[:100]}...".split("\n")[0] if len(message.clean_content) > 100 else message.clean_content.split("\n")[0]
-        custom(f'[Deleted Message] Author: {message.author} | Channel: {f"#{message.channel.name}" if isinstance(message.channel, discord.TextChannel) else f"DM with {message.author.name}"} | Content: {msg}', color = "bright_red")
+    if (
+        message.channel.id in data['blacklisted_channels']
+        or len(message.clean_content) == 0
+        or message.author.id == self.bot.user.id
+        or message.author.id in data['owner_ids']
+    ):
+      return
+    if (
+        data['ignore_bots'] is True
+        and message.author.bot
+    ):
+      return
+    table = Table(
+      title="Message Deletion Logger \[Guild]" if isinstance(message.channel, discord.TextChannel) else "Message Deletion Logger \[DM]",
+      caption = f"Run {data['prefix']}deleted to disable this feature\n",
+      box = box.HEAVY,
+      style = "bold red"
+    )
+    if isinstance(message.channel, discord.TextChannel):
+      table.add_column("Message Author", justify = "center")
+      table.add_column("Channel", justify = "center")
+      table.add_column("Content", justify = "center")
+      table.add_column("Channel ID", justify = "center")
+      table.add_column("Guild Name", justify = "center")
+      table.add_row(str(message.author), f"#{str(message.channel.name)}", str(message.clean_content), str(message.channel.id), str(message.guild.name))
+    else:
+      table.add_column("DM Author", justify = "center")
+      table.add_column("Content", justify = "center")
+      table.add_column("Channel ID", justify = "center")
+      table.add_row(str(message.author), str(message.clean_content), str(message.channel.id))
+
+    if (
+        data['log_all'] is True
+    ):
+      print(table)
+    else:
+      if (
+          message.channel.id in data['logged_channels']
+          or message.guild.id in data['logged_guilds']
+      ):
+        print(table)
 
   @Cog.listener("on_message")
   async def anti_scam_links(self, message : discord.Message):
-    if (self.link_regex.search(message.clean_content) is not None
-    and message.author.id != self.bot.user.id
-    and message.author.id not in self.data['owner_ids']):
+    data = get_data()
+    if data['antiscam'] is False:
+      return
+    if (
+        self.link_regex.search(message.clean_content) is not None
+        and message.author.id != self.bot.user.id
+        and message.author.id not in data['owner_ids']
+    ):
       link = self.link_regex.search(message.clean_content).group()
       if link is None:
         return
       link_ = link.replace('https://', '').replace('http://', '').replace('/', '')
+      table = Table(
+        title="Anti Scam Links \[Guild]" if isinstance(message.channel, discord.TextChannel) else "Anti Scam Links \[DM]",
+        caption = f"Run {data['prefix']}antiscam to disable this feature\n",
+        box = box.HEAVY,
+        style = "bold magenta"
+      )
       for index, splitted in enumerate(link_.split(' '), start = 0):
         if splitted in self.scam_links['domains']:
-          custom(f"[Anti Scam Links] Found a scam link sent by {message.author}. Link: {link.split(' ')[index]}", color = 'magenta')
-          await message.author.block()
-          return
+          table.add_column("Message Author", justify = "center")
+          table.add_column("Scam Link", justify = "center")
+          table.add_column("Message ID", justify = "center")
+          table.add_row(str(message.author), link.split(' ')[index], str(message.id))
+          print(table)
+          return await message.author.block()
 
 async def setup(bot):
   await bot.add_cog(events(bot))
